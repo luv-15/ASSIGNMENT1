817598b4bf7f8bd1669ed243bdb0813ba5f472bf
luv-15 <luvkumar9540@gmail.com>
2025-07-24 20:11:55 +0530

    Add files via upload

---
+++ task1.py
@@ -0,0 +1,14 @@
+a=float(input("enter first number:"))
+b=float(input("enter second number:"))
+add=a+b
+sub=a-b
+multply=a*b
+if b!=0:
+  divide=a/b
+else :
+  divide= 'not divisible by zero'
+
+print("\nAddition=",add)
+print("Subtraction=",sub)
+print("Multiplication=",multply)
+print("Division=",divide)
